8c353e4a25a33ac6ef6b2fbf672d08eacf7fcca1
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2024-12-14 18:57:37 +0100

    ok

---
+++ Day3_1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+var test = @"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+
+var matches = Regex.Matches(test, @"mul\((\d{1,3}),(\d{1,3})\)");
+
+foreach (Match match in matches)
+{
+    match.Dump();
+    Console.WriteLine(match.Value);
+}
