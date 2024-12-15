5b500ec99fbec489eb05f5a45bb1736ab87ff65d
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2024-12-15 01:03:09 +0100

    ok

--- Visu/wwwroot/day15.js
+++ Visu/wwwroot/day15.js
@@ -1,35 +1,103 @@
-﻿window.day15 = {
+﻿// https://webglfundamentals.org/webgl/lessons/webgl-fundamentals.html
+
+window.day15 = {
     initializeScene: (canvasId) => {
+
+        function createShader(gl, type, source) {
+            var shader = gl.createShader(type);
+            gl.shaderSource(shader, source);
+            gl.compileShader(shader);
+            var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+            if (success) {
+                return shader;
+            }
+
+            console.log(gl.getShaderInfoLog(shader));
+            gl.deleteShader(shader);
+        }
+        function createProgram(gl, vertexShader, fragmentShader) {
+            var program = gl.createProgram();
+            gl.attachShader(program, vertexShader);
+            gl.attachShader(program, fragmentShader);
+            gl.linkProgram(program);
+            var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+            if (success) {
+                return program;
+            }
+
+            console.log(gl.getProgramInfoLog(program));
+            gl.deleteProgram(program);
+        }
+
+        function resizeCanvasToDisplaySize(canvas) {
+            // Lookup the size the browser is displaying the canvas in CSS pixels.
+            const displayWidth = canvas.clientWidth;
+            const displayHeight = canvas.clientHeight;
+
+            // Check if the canvas is not the same size.
+            const needResize = canvas.width !== displayWidth ||
+                canvas.height !== displayHeight;
+
+            if (needResize) {
+                // Make the canvas the same size
+                canvas.width = displayWidth;
+                canvas.height = displayHeight;
+            }
+            return needResize;
+        }
+
         const canvas = document.getElementById(canvasId);
-        const width = canvas.clientWidth;
-        const height = canvas.clientHeight;
-        const renderer = new THREE.WebGLRenderer({ canvas });
-        renderer.setSize(width, height, false);
+        var gl = canvas.getContext("webgl");
+        if (!gl)
+            return;
+        resizeCanvasToDisplaySize(gl.canvas);
+        var vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
+        var fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
 
-        const scene = new THREE.Scene();
-        scene.background = new THREE.Color(0xffffff);
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        camera.position.z = 3;
+        var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+        var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+        var program = createProgram(gl, vertexShader, fragmentShader);
+        var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+        var positionBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+        var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
 
-        const geometry = new THREE.BoxGeometry();
-        const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
-        const cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
-        // Add directional light (like sunlight)
-        const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-        directionalLight.position.set(5, 5, 5); // x, y, z
-        scene.add(directionalLight);
+        var positions = [
+            0, 0,
+            gl.canvas.width - 1,0,
+            gl.canvas.width-1, gl.canvas.height-1,
+            gl.canvas.width - 1, gl.canvas.height - 1,
+            0, gl.canvas.height - 1,
+            0, 0
+        ];
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+        // Clear the canvas
+        gl.clearColor(0, 0, 0, 0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        gl.useProgram(program);
+        gl.enableVertexAttribArray(positionAttributeLocation);
+        // Bind the position buffer.
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+        // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+        var size = 2;          // 2 components per iteration
+        var type = gl.FLOAT;   // the data is 32bit floats
+        var normalize = false; // don't normalize the data
+        var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+        var offset = 0;        // start at the beginning of the buffer
+        gl.vertexAttribPointer(
+            positionAttributeLocation, size, type, normalize, stride, offset);
+        gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+
+        var primitiveType = gl.TRIANGLES;
+        var offset = 0;
+        var count = 6;
+        gl.drawArrays(primitiveType, offset, count);
+
         function animate() {
             requestAnimationFrame(animate);
-            var value = 0.01;
-            DotNet.invokeMethodAsync('Visu', 'UpdateValueDay15').then(
-                result => {
-                    value = result;
-                    cube.rotation.x = value;
-                    cube.rotation.y = value;
-                    renderer.render(scene, camera);
-                }
-            )                ;
         }
         animate();
     }
