2f88bd41b75b8c9b9627fc976016743108412593
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2024-12-16 23:05:46 +0100

    ok

--- Day16_2/Solution.cs
+++ Day16_2/Solution.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Runtime.CompilerServices;
+using System.Text;
 using static System.Net.Mime.MediaTypeNames;
 
 internal class Solution
@@ -19,9 +21,35 @@ internal class Solution
 
     void Visu(HashSet<(int x, int y)> visited)
     {
+        string NL = Environment.NewLine; // shortcut
+        string NORMAL = Console.IsOutputRedirected ? "" : "\x1b[39m";
+        string RED = Console.IsOutputRedirected ? "" : "\x1b[91m";
+        string GREEN = Console.IsOutputRedirected ? "" : "\x1b[92m";
+        string YELLOW = Console.IsOutputRedirected ? "" : "\x1b[93m";
+        string BLUE = Console.IsOutputRedirected ? "" : "\x1b[94m";
+        string MAGENTA = Console.IsOutputRedirected ? "" : "\x1b[95m";
+        string CYAN = Console.IsOutputRedirected ? "" : "\x1b[96m";
+        string GREY = Console.IsOutputRedirected ? "" : "\x1b[97m";
+        string BOLD = Console.IsOutputRedirected ? "" : "\x1b[1m";
+        string NOBOLD = Console.IsOutputRedirected ? "" : "\x1b[22m";
+        string UNDERLINE = Console.IsOutputRedirected ? "" : "\x1b[4m";
+        string NOUNDERLINE = Console.IsOutputRedirected ? "" : "\x1b[24m";
+        string REVERSE = Console.IsOutputRedirected ? "" : "\x1b[7m";
+        string NOREVERSE = Console.IsOutputRedirected ? "" : "\x1b[27m";
+
         Console.WriteLine();
         foreach (var item in map.Select((row, idx) => (row, y: idx)))
-            Console.WriteLine(new string(item.row.Select((c, x) => visited.Contains((x, item.y)) ? 'O' : c).ToArray()));
+        {
+            var sb = new StringBuilder();
+            for (var x = 0; x < item.row.Length;x++)
+            {
+                if ( visited.Contains((x, item.y)))
+                    sb.Append($"{RED}O{NORMAL}");
+                else
+                    sb.Append(item.row[x]); 
+            }
+            Console.WriteLine(sb.ToString());
+        }
     }
     internal long Run()
     {
