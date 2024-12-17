f184765a6586159d19f67c265a0f31bcc0689c21
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2024-12-17 13:56:34 +0100

    part 1

---
+++ Day17_1/Program.cs
@@ -0,0 +1,17 @@
+﻿var test2 = @"Register A: 22817223
+Register B: 0
+Register C: 0
+
+Program: 2,4,1,2,7,5,4,5,0,3,1,7,5,5,3,0";
+
+var test = @"Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0";
+
+//4,4,7,1,1,0,0,7,4
+//4,3,7,1,5,3,0,5,4
+var solution = new Solution(test2);
+
+Console.WriteLine(solution.Run());
---
+++ Day17_1/Solution.cs
@@ -0,0 +1,80 @@
+﻿
+using System.Text.RegularExpressions;
+
+internal class Solution
+{
+    private readonly long[] registers;
+    private readonly int[] prog;
+    public Solution(string test)
+    {
+        var parts = test.Replace("\r",string.Empty).Split("\n\n");
+        registers = parts[0].Split("\n")
+            .Select(x => long.Parse(Regex.Match(x, @"Register .: (\d+)").Groups[1].Value)).ToArray();
+        prog = parts[1].Replace("Program: ",string.Empty).Split(",").Select(x => int.Parse(x)).ToArray();
+    }
+
+    internal string Run()
+    {
+        var pointer = 0;
+        var output = new List<long>();
+        while (pointer < prog.Length)
+        {
+            var inst = prog[pointer];
+            var op = prog[pointer + 1];
+            if ( inst == 0) // adv
+            {
+                registers[0] = registers[0] >> (int)Combo(op);
+                pointer += 2;
+            }
+            if (inst == 1) // bxl
+            {
+                registers[1] = registers[1] ^ op;
+                pointer += 2;
+            }
+            if (inst == 2) // bst
+            {
+                registers[1] = Combo(op) % 8;
+                pointer += 2;
+            }
+            if (inst == 3) // jnz
+            {
+                if (registers[0] != 0)
+                {
+                    pointer = op;
+                }
+                else 
+                    pointer += 2;
+            }
+            if (inst == 4) // bxc
+            {
+                registers[1] = registers[1] ^ registers[2];
+                pointer += 2;
+            }
+            if (inst == 5) // out
+            {
+                output.Add(Combo(op) % 8);
+                pointer += 2;
+            }
+            if (inst == 6) // bdv
+            {
+                registers[1] = registers[0] >> (int)Combo(op);
+                pointer += 2;
+            }
+            if (inst == 7) // cdv
+            {
+                registers[2] = registers[0] >> (int)Combo(op);
+                pointer += 2;
+            }
+
+
+        }
+
+        return string.Join(',',output);
+    }
+
+    private long Combo(int op) => op switch
+    {
+        <= 3 => op,
+        _ => registers[op-4]
+    };
+}
